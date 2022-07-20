4e9965cf2027091eb7cfd286a86c95a567bb0a43
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-07-20 11:49:02 -0300

    exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-1.js
@@ -0,0 +1,33 @@
+// Exercicio 1
+
+function adicao (a, b) {
+  console.log(a + b)
+}
+
+adicao(21, 4)
+
+function subtracao (a, b) {
+  console.log(a - b)
+}
+
+subtracao(21, 4)
+
+function multiplicacao (a, b) {
+  console.log(a * b)
+}
+
+multiplicacao(21, 4)
+
+function divisao (a, b) {
+  console.log(a / b)
+}
+
+divisao(21, 4)
+
+function modulo(a, b) {
+  console.log(a % b)
+}
+
+modulo(21, 4)
+
+
