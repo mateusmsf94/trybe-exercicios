3e8a547e180e6a318c8768b07b3bdfee5eb146b2
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-07-25 18:29:39 -0300

    exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/objetos-e-for-in.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/objetos-e-for-in.js
@@ -4,4 +4,11 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+// Exercicio 1
 console.log('Bem-vinda,', info.personagem)
+
+
+// Exercicio 2
+info.recorrente = 'Sim';
+
+console.log(info)
