ffb092d8ca6d49d09271e3ea0af34cfd7123d840
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-07-26 10:41:12 -0300

    exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/objetos-e-for-in.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/objetos-e-for-in.js
@@ -55,3 +55,12 @@ let leitor = {
 };
 
 console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}`)
+
+//Exercicio 7
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+})
+
+console.log(leitor.livrosFavoritos)
